44f279434bb0c5ff1970d1a270225e7e88bf5a01
Roman Serikoff <serikoff@li.ru>
2021-01-10 00:23:51 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import dedicatedWorkerUrl from "worker-plugin/loader!./workers/get-nth-fibonacci-number.worker.js";
-import { getNthFobonacciNumber } from "./helpers/get-nth-fibonacci-number";
-import { initWorker, initWorkerFn } from "./promisified-worker";
+import dedicatedWorkerUrl from 'worker-plugin/loader!./workers/get-nth-fibonacci-number.worker.js';
+import { getNthFobonacciNumber } from './helpers/get-nth-fibonacci-number';
+import { initWorker, initWorkerFn } from './promisified-worker';
 
-import { initFibonacciSharedWorker } from "./init-fibonacci-shared-worker";
-import { initFibonacciDedicatedWorker } from "./init-fibonacci-dedicated-worker";
+import { initFibonacciSharedWorker } from './init-fibonacci-shared-worker';
+import { initFibonacciDedicatedWorker } from './init-fibonacci-dedicated-worker';
 
-import { button, input, result } from "./elements";
-import "./styles/style.scss";
+import { button, input, result } from './elements';
+import './styles/style.scss';
 
 // Shared worker
 const sharedWorker = initFibonacciSharedWorker(
-  (data) => {
-    result.innerHTML = JSON.stringify(data, null, 2);
-  },
-  (errorMessage) => {
-    result.innerHTML = errorMessage;
-  }
+	(data) => {
+		result.innerHTML = JSON.stringify(data, null, 2);
+	},
+	(errorMessage) => {
+		result.innerHTML = errorMessage;
+	},
 );
 
 // Dedicated worker
 const dedicatedWorker = initFibonacciDedicatedWorker(
-  (data) => {
-    result.innerHTML = data;
-  },
-  (errorMessage) => {
-    result.innerHTML = errorMessage;
-  }
+	(data) => {
+		result.innerHTML = data;
+	},
+	(errorMessage) => {
+		result.innerHTML = errorMessage;
+	},
 );
 
 // promisified
@@ -37,30 +37,30 @@ const runPromisifiedWorkerFn = initWorkerFn(getNthFobonacciNumber);
 // app
 let n = null;
 
-input.addEventListener("change", (e) => {
-  n = e.target.value;
+input.addEventListener('change', (e) => {
+	n = e.target.value;
 });
 
-button.addEventListener("click", (e) => {
-  result.innerHTML = "processing...";
+button.addEventListener('click', async (e) => {
+	result.innerHTML = 'processing...';
 
-  // sharedWorker.port.postMessage(n);
+	// sharedWorker.port.postMessage(n);
 
-  // dedicatedWorker.postMessage(n);
+	// dedicatedWorker.postMessage(n);
 
-  runPromisifiedWorker(n)
-    .then((value) => {
-      result.innerHTML = value;
-    })
-    .catch((errorMessage) => {
-      result.innerHTML = errorMessage;
-    });
+	try {
+		const workerResult = await runPromisifiedWorker(n);
+		result.innerHTML = workerResult;
+	} catch (error) {
+		result.innerHTML = errorMessage;
+	}
 
-  // runPromisifiedWorkerFn(n)
-  //   .then((value) => {
-  //     result.innerHTML = value;
-  //   })
-  //   .catch((errorMessage) => {
-  //     result.innerHTML = errorMessage;
-  //   });
+
+	// runPromisifiedWorkerFn(n)
+	//   .then((value) => {
+	//     result.innerHTML = value;
+	//   })
+	//   .catch((errorMessage) => {
+	//     result.innerHTML = errorMessage;
+	//   });
 });
